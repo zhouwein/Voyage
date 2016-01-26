20c7299ae2be1270b466f96c1c32d42e5f64b83e
zhouwein <william.zhou@mail.utoronto.ca>
2016-01-26 17:31:26 -0500

    .

--- Frontend/Crawler/models.py
+++ Frontend/Crawler/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+import hashlib
 
 class Page(models.Model):
-    url = models.URLField(max_length=2000, required=True)
-    urlHash = models.IntegerField(required=True)
-    site = models.ForeignKey('explorer.ReferringSite', on_delete=models.CASCADE, required=True)
+    url = models.URLField(max_length=2000)
+    urlHash = models.BigIntegerField()
+
+    '''def save( self, *args, **kw ):
+        m = hashlib.md5()
+        m.update(self.url)
+        self.urlHash = long(m.hexdigest(), 16)
+        super(Page, self).save( *args, **kw )'''
 
 class VisitedPage(Page):
     pass
