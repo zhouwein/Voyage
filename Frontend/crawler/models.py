baaa64f08ec8875e51139ca8d077d59b83a6b648
zhouwein <william.zhou@mail.utoronto.ca>
2016-01-26 23:56:56 -0500

    .

--- Frontend/crawler/models.py
+++ Frontend/crawler/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from explorer.models import SourceSite
 import hashlib
 
 class Page(models.Model):
     url = models.URLField(max_length=2000)
     urlHash = models.BigIntegerField()
-
+    sourceSite = models.ForeignKey(SourceSite)
     '''def save( self, *args, **kw ):
         m = hashlib.md5()
         m.update(self.url)
