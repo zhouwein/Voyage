a740c4ccc755c0d5241c110275da229431e8c4d3
zhouwein <zhouwein@users.noreply.github.com>
2016-01-27 23:39:10 -0500

    .

--- src/replay_django.py
+++ src/replay_django.py
@@ -2,38 +2,66 @@
 import sys
 import os
 import time
+import codecs
+import hashlib
+import struct
+import MySQLdb
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','Frontend')))
-import django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Frontend.settings'
-from crawler.models import VisitedPage
-from crawler.models import ToVisitPage
-from explorer.models import SourceSite
+
+def hash(str):
+    h = hashlib.md5()
+    h.update(str)
+    return struct.unpack('q', h.digest()[:8])[0]
 
 if __name__ == "__main__":
-    django.setup()
-    VisitedPage.objects.all().delete()
-    ToVisitPage.objects.all().delete()
-    SourceSite.objects.all().delete()
-
-    source_site = SourceSite(url="example.com", name="example")
-    source_site.save()
-    ToVisitPage(url="example.com", urlHash=0, sourceSite=source_site).save()
+    db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="voyage", charset="utf8")
+    c = db.cursor()
+    f = codecs.open("/mnt/bbc", "r", "utf-8")
 
     start = time.time()
-    for line in sys.stdin:
+    t0 = 0
+    t1 = 0
+    t2 = 0
+    i = 0
+    for line in f:
         type = line[0]
 
         if(type == "0"):
-            page = ToVisitPage.objects.filter(sourceSite=source_site).first()
-            page.delete()
+            s0 = time.time()
+            if(c.execute("select * from tovisit order by id limit 1")):
+                id = c.fetchone()[0]
+                c.execute("delete from tovisit where id=%s", (id,))
+            else:
+                print "nothing to pop!"
+
+            t0 += time.time() - s0
             continue
         arg = line[2:].strip()
         if(type == "1"):
-            VisitedPage.objects.filter(url=arg, sourceSite=source_site).count()
+            s1 = time.time()
+            c.execute(u"select exists(select * from visited where url=%s)",(arg,))
+            c.fetchone()
+            t1 += time.time() - s1
         elif(type == "2"):
-            VisitedPage(url=arg, urlHash=0, sourceSite=source_site).save()
-            ToVisitPage(url=arg, urlHash=0, sourceSite=source_site).save()
+            s2 = time.time()
+            try:
+                c.execute(u"insert into visited values (%s)", (arg,))
+            except MySQLdb.IntegrityError:
+                pass
+            c.execute(u"insert into tovisit values (default, %s)", [arg])
+            t2 += time.time() - s2
+        i+=1
+        if(i > 10000000):
+            break
+        if i % 10000 == 0:
+            print i
+            db.commit()
+    c.close()
+    db.commit()
+    db.close()
 
     print "elapsed time: " + str((time.time() - start))
+    print "t0:", t0
+    print "t1:", t1
+    print "t2:", t2
+    
